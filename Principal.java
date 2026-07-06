cfc2593e8a01ebba2201658446d36f7d4655d20e
lhenaov <lhenaov@unal.edu.co>
2026-07-06 16:33:38 -0500

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo_t6;
+
+
+public class POO_T6 {
+
+    public static void main(String[] args) {
+        interfaz ventana = new interfaz();
+        ventana.setVisible(true);
+    }
+}
